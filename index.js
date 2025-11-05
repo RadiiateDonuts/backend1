93edef57c89009454988f73bc781279130f6360f
humberto <rolh_2003@outlook.com>
2025-11-05 17:15:55 -0600

    Agregar puerto

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const badPath = (request, response, next) => {
 
 app.use(badPath)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
